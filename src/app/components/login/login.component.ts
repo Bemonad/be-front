4ae6f6de476cf13b6e6e05c84631fb63966031aa
Vincent Schlosser <s.vincent@live.fr>
2020-04-10 10:44:49 +0200

    Manage login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  login: {
+    email: string;
+    password: string;
+  };
 
-  constructor() { }
+  constructor(private userService: UserService) {
+    this.login = {
+      email: '',
+      password: ''
+    };
+  }
 
   ngOnInit(): void {
+  }
+
+  logIn() {
+    console.log(this.login);
+    this.userService.checkCredentials(this.login).subscribe((user) => {
+      this.userService.finalCheckIn(user);
+    });
   }
 
 }
