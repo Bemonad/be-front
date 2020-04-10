108950eb67f50ad7389979ed6ab5b2c1b538a790
Vincent Schlosser <s.vincent@live.fr>
2020-04-10 10:44:49 +0200

    Manage login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,35 +1,52 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 interface UserData {
+  _id: string;
   firstName: string;
   lastName: string;
   email: string;
-  _id: string;
+  role: string;
+  token: string;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  user: UserData
-  constructor(private http: HttpClient) {}
+  user: UserData;
+  apiUrl: string;
+  httpOptionsJson: object;
 
-  getUser(jwt) {
-    // @TODO: Use env var for base URL
-    return this.http.get<UserData>(`http://localhost:3001/api/users/${jwt}`);
-
-  }
-
-  register(user, password) {
-    const httpOptions = {
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.API_URL;
+    this.httpOptionsJson = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json'
       })
     };
+  }
 
+  getUser(jwt) {
+    // @TODO: Use env var for base URL
+    return this.http.get<UserData>(`${this.apiUrl}/users/${jwt}`);
+
+  }
+
+  register(user, password) {
     const body = {firstName: user.firstName, lastName: user.lastName, password, token: user.token};
 
-    return this.http.put<UserData>(`http://localhost:3001/api/users`, body, httpOptions);    
+    return this.http.put<UserData>(`${this.apiUrl}/users`, body, this.httpOptionsJson);
+  }
+
+  checkCredentials(loginData) {
+    console.log(loginData);
+    return this.http.post(`${this.apiUrl}/users/login`, loginData, this.httpOptionsJson );
+  }
+
+  finalCheckIn(user) {
+    this.user = user;
+    localStorage.setItem('access_token', this.user.token);
   }
 }
