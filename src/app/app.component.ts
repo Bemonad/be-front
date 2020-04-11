c527e4a3cfa12bd0e75d54e2ae87d2d1e0272f57
Félix Darraud <felix.darraud@gmail.com>
2020-04-11 18:22:33 +0200

    Add mobile nav structure + Skewmorphic shadow mixin + refactored sass variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'bemonad-client';
+  title = "bemonad-client";
+  mobileNav = {
+    isOpen: false,
+  };
+
+  toggleMobileMenu = () => {
+    this.mobileNav.isOpen = !this.mobileNav.isOpen;
+    console.log(this.mobileNav.isOpen);
+  }
 }
