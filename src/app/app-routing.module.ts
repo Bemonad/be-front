eaa7c0a520143aad3ffaf51dc5d4f7f2f5849cc1
Vincent Schlosser <s.vincent@live.fr>
2020-04-11 16:16:42 +0200

    Create routes associated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RoomComponent } from './components/room/room.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { MyBookingsComponent } from './components/my-bookings/my-bookings.component';
 import { AuthGardService} from './services/auth-gard.service';
 
 const routes: Routes = [
@@ -15,10 +18,22 @@ const routes: Routes = [
     component: LoginComponent,
   },
   {
+    path: 'rooms/:id',
+    component: RoomComponent,
+  },
+  {
+    path: 'booking',
+    component: BookingComponent,
+  },
+  {
+    path: 'my-bookings',
+    component: MyBookingsComponent,
+  },
+  {
     path: '',
     component: HomeComponent,
     // canActivate: [AuthGardService] : Example
-  }
+  },
 ];
 
 @NgModule({
