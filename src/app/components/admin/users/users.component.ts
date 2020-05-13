6ed97633ffa26b649b1ae4db0be15375051317d8
Vincent Schlosser <s.vincent@live.fr>
2020-05-13 20:37:04 +0200

    Finalise user management

--- src/app/components/admin/users/users.component.ts
+++ src/app/components/admin/users/users.component.ts
@@ -10,6 +10,7 @@ export class UsersComponent implements OnInit {
   public allUsers: Array<UserData>;
   public action: string;
   public addUser: { firstName: string; lastName: string; role: string; email: string };
+  public userToEdit: UserData;
 
   constructor(private userService: UserService) { }
 
@@ -20,23 +21,48 @@ export class UsersComponent implements OnInit {
       email: '',
       role: '',
     };
+    this.userToEdit = {
+      _id: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      role: '',
+      token: ''
+    }
     this.action = 'show';
     this.userService.getAllUsers().subscribe( users => {
       this.allUsers = users;
     });
   }
 
-  changeAction(action) {
+  changeAction(action, data) {
     if (action === 'addUser') {
       this.action = 'addUser';
+    }
+    if (action === 'editUser') {
+      this.userToEdit = data;
+      this.action = 'editUser';
     }
   }
 
   addUserAction() {
     this.userService.createUser(this.addUser).subscribe(response => {
       console.log(response);
+      this.userService.getAllUsers().subscribe( users => {
+        this.allUsers = users;
+      });
     });
-    this.action = 'addUser';
+    this.action = 'show';
+  }
+
+  editUserAction() {
+    this.userService.updateUser(this.userToEdit).subscribe(response => {
+      console.log(response);
+      this.userService.getAllUsers().subscribe( users => {
+        this.allUsers = users;
+      });
+    });
+    this.action = 'show';
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -89,4 +89,8 @@ export class UserService {
     return this.http.post(`${this.apiUrl}/users`, body, this.httpOptionsJson);
   }
 
+  updateUser(body) {
+    return this.http.put(`${this.apiUrl}/users`, body, this.httpOptionsJson);
+  }
+
 }
