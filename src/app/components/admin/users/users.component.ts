d982d88326ab3b3c967c52d5045437aa6f338e76
Félix Darraud <felix.darraud@gmail.com>
2020-05-24 17:57:01 +0200

    Added style for users listing

--- src/app/components/admin/users/users.component.ts
+++ src/app/components/admin/users/users.component.ts
@@ -1,68 +1,93 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService, UserData } from '../../../services/user.service';
+import { Component, OnInit } from "@angular/core";
+import { UserService, UserData } from "../../../services/user.service";
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.scss"],
 })
 export class UsersComponent implements OnInit {
   public allUsers: Array<UserData>;
-  public action: string;
-  public addUser: { firstName: string; lastName: string; role: string; email: string };
-  public userToEdit: UserData;
+  public filteredUsers: Array<UserData> = this.allUsers;
+  public emailFilter: string = '';
 
-  constructor(private userService: UserService) { }
+  public action: string = "show";
+
+  public addUser: {
+    firstName: string;
+    lastName: string;
+    role: string;
+    email: string;
+  } = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    role: "",
+  };
+
+  public userToEdit: UserData = {
+    _id: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    role: "",
+    token: "",
+  };
+
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.addUser = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      role: '',
-    };
-    this.userToEdit = {
-      _id: '',
-      firstName: '',
-      lastName: '',
-      email: '',
-      role: '',
-      token: ''
-    }
-    this.action = 'show';
-    this.userService.getAllUsers().subscribe( users => {
+    this.userService.getAllUsers().subscribe((users) => {
       this.allUsers = users;
+      this.filteredUsers = this.allUsers;
     });
   }
 
   changeAction(action, data) {
-    if (action === 'addUser') {
-      this.action = 'addUser';
+    if (action === "show") {
+      this.action = "show";
     }
-    if (action === 'editUser') {
+    if (action === "addUser") {
+      this.action = "addUser";
+    }
+    if (action === "editUser") {
       this.userToEdit = data;
-      this.action = 'editUser';
+      this.action = "editUser";
     }
   }
 
   addUserAction() {
-    this.userService.createUser(this.addUser).subscribe(response => {
+    this.userService.createUser(this.addUser).subscribe((response) => {
       console.log(response);
-      this.userService.getAllUsers().subscribe( users => {
+      this.userService.getAllUsers().subscribe((users) => {
         this.allUsers = users;
+        this.resetFilter();
       });
     });
-    this.action = 'show';
+    this.action = "show";
   }
 
   editUserAction() {
-    this.userService.updateUser(this.userToEdit).subscribe(response => {
-      console.log(response);
-      this.userService.getAllUsers().subscribe( users => {
+    this.userService.updateUser(this.userToEdit).subscribe((response) => {
+      this.userService.getAllUsers().subscribe((users) => {
         this.allUsers = users;
+        this.resetFilter();
       });
     });
-    this.action = 'show';
+    this.action = "show";
   }
 
+  resetFilter() {
+    this.emailFilter = '';
+  }
+
+  filterUsers() {
+    if (this.emailFilter) {
+      this.filteredUsers = this.allUsers.filter((user) => {
+        return user.email.toLowerCase().startsWith(this.emailFilter.toLowerCase());
+      })
+    } else {
+      this.filteredUsers = this.allUsers;
+    }
+  }
 }
